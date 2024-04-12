8efe6b0adbd5c4c5548a3823fff40d36dc9392e5
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-04-13 01:29:21 +0530

    Functions of suggestions boxes improved

--- src/container/SearchContainer.tsx
+++ src/container/SearchContainer.tsx
@@ -2,12 +2,41 @@ import { useState } from "react"
 import { BiPaperPlane, BiSearch } from "react-icons/bi"
 import { GrLocation } from "react-icons/gr"
 import { Link } from "react-router-dom"
+import { companiesName, locationsName } from "../DataList/companiesAndLocationDataList"
 
 function SearchContainer() {
-  const [searchCompany, setSearchCompany] = useState("")
-  const [searchLocation, setSearchLocation] = useState("")
-  const [isCompanyVisible, setIsCompanyVisible] = useState(false)
-  const [isLocationVisible, setIsLocationVisible] = useState(false)
+  const [searchCompany, setSearchCompany] = useState<string>("")
+  const [searchLocation, setSearchLocation] = useState<string>("")
+  const [isCompanyVisible, setIsCompanyVisible] = useState<Boolean>(false)
+  const [isLocationVisible, setIsLocationVisible] = useState<Boolean>(false)
+  const [suggestedCompanies, setSuggestedCompanies] = useState<string[]>([])
+  const [suggestedLocations, setSuggestedLocations] = useState<string[]>([])
+
+  const handleCompaniesInputChange = (value: string) => {
+    setSearchCompany(value)
+    const filteredCompanies = companiesName
+      .filter((company) => company.companyName.toLowerCase().includes(value.toLowerCase()))
+      .map((company) => company.companyName)
+
+    setSuggestedCompanies(filteredCompanies)
+  }
+
+  const handleLocationInputChange = (value: string) => {
+    setSearchLocation(value)
+    const filteredLocation = locationsName
+      .filter((location) => location.locationName.toLowerCase().includes(value.toLowerCase()))
+      .map(company => company.locationName)
+      
+    setSuggestedLocations(filteredLocation)
+  }
+
+  const checkCompanyField = () => {
+    return searchCompany.length === 0 ? "hidden" : "block"
+  }
+
+  const checkLocationField = () => {
+    return searchLocation.length === 0 ? "hidden" : "block"
+  }  
 
   return (
     <section className="w-full p-10 bg-blue-500 flex flex-col justify-center items-center">
@@ -19,18 +48,19 @@ function SearchContainer() {
             placeholder="Company name or industry"
             className="w-full outline-none h-10 px-2 font-sans"
             value={searchCompany}
-            onChange={(e) => setSearchCompany(e.target.value)}
+            onChange={(e) => handleCompaniesInputChange(e.target.value)}
             onFocus={() => setIsCompanyVisible(true)}
             onBlur={() => setIsCompanyVisible(false)}
           />
           <div>
             <ul
-              className={`absolute z-10 ${isCompanyVisible ? "block": "hidden"} top-3 left-0 w-full bg-white rounded-lg shadow-md mt-12 focus:block`}
+              className={`absolute z-10 ${isCompanyVisible ? "block" : "hidden"} ${checkCompanyField()} h-auto max-h-[25rem] top-3 left-0 w-full bg-white rounded-lg shadow-md mt-12 focus:block overflow-y-auto`}
             >
-              <li className="p-2 hover:bg-gray-100 cursor-pointer">Company 1</li>
-              <li className="p-2 hover:bg-gray-100 cursor-pointer">Company 2</li>
-              <li className="p-2 hover:bg-gray-100 cursor-pointer">Company 3</li>
-              <li className="p-2 hover:bg-gray-100 cursor-pointer">Company 4</li>
+              {suggestedCompanies.map((cname, index) => (
+                <li key={index} className="p-2 hover:bg-gray-100 cursor-pointer">
+                  {cname}
+                </li>
+              ))}
             </ul>
           </div>
         </div>
@@ -41,18 +71,19 @@ function SearchContainer() {
             placeholder="Location name"
             className="w-full outline-none h-10 px-2"
             value={searchLocation}
-            onChange={(e) => setSearchLocation(e.target.value)}
+            onChange={(e) => handleLocationInputChange(e.target.value)}
             onFocus={() => setIsLocationVisible(true)}
             onBlur={() => setIsLocationVisible(false)}
           />
           <div>
             <ul
-              className={`absolute z-10 ${isLocationVisible ? "block": "hidden"} top-3 left-0 w-full bg-white rounded-lg shadow-md mt-12 focus:block`}
+              className={`absolute z-10 ${isLocationVisible ? "block" : "hidden"} ${checkLocationField()} h-auto max-h-[25rem] top-3 left-0 w-full bg-white rounded-lg shadow-md mt-12 focus:block overflow-y-auto`}
             >
-              <li className="p-2 hover:bg-gray-100 cursor-pointer">Location 1</li>
-              <li className="p-2 hover:bg-gray-100 cursor-pointer">Location 2</li>
-              <li className="p-2 hover:bg-gray-100 cursor-pointer">Location 3</li>
-              <li className="p-2 hover:bg-gray-100 cursor-pointer">Location 4</li>
+              {suggestedLocations.map((lname, index) => (
+                <li key={index} className="p-2 hover:bg-gray-100 cursor-pointer">
+                  {lname}
+                </li>
+              ))}
             </ul>
           </div>
         </div>

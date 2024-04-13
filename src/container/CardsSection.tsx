7bbe9f8c882944cb3a122fcabb9f9ccb09d139e4
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-04-14 00:11:00 +0530

    Added a spinner

--- src/container/CardsSection.tsx
+++ src/container/CardsSection.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import CompanyCard from "./CompanyCard";
 import { companiesData } from "../DataList/companiesDataList";
 import InfiniteScroll from "react-infinite-scroll-component";
+import Spinner from "./Spinner";
 
 function CardsSection() {
   const [visibleCompanies, setVisibleCompanies] = useState(companiesData.slice(0, 5));
@@ -11,7 +12,7 @@ function CardsSection() {
     setTimeout(() => {
       const nextCompanies = companiesData.slice(
         visibleCompanies.length,
-        visibleCompanies.length + 5 
+        visibleCompanies.length + 5
       );
       if (nextCompanies.length === 0) {
         setHasMore(false);
@@ -22,27 +23,30 @@ function CardsSection() {
   };
 
   return (
-    <InfiniteScroll
-      dataLength={visibleCompanies.length}
-      next={fetchMoreData}
-      hasMore={hasMore}
-      loader={<h4>Loading...</h4>}
-      endMessage={
-        <p className="text-center text-2xl pb-4 font-semibold">No more companies to load</p>
-      }
-    >
-      {visibleCompanies.map((company) => (
-        company.locations.map((location, index) => (
-          <CompanyCard
-            key={`${company.id}-${index}`}
-            companyName={company.companyName}
-            headquarter={company.headquarter}
-            location={location}
-            industry={company.industry}
-          />
-        ))
-      ))}
-    </InfiniteScroll>
+    <>
+      <InfiniteScroll
+        className="w-full flex flex-wrap justify-center items-center gap-4 p-4"
+        dataLength={visibleCompanies.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={<Spinner />}
+        endMessage={
+          <p className="text-center text-2xl pb-4 font-semibold">No more companies to load</p>
+        }
+      >
+        {visibleCompanies.map((company) => (
+          company.locations.map((location, index) => (
+            <CompanyCard
+              key={`${company.id}-${index}`}
+              companyName={company.companyName}
+              headquarter={company.headquarter}
+              location={location}
+              industry={company.industry}
+            />
+          ))
+        ))}
+      </InfiniteScroll>
+    </>
   );
 }
 
---
+++ src/container/Spinner.tsx
@@ -0,0 +1,13 @@
+function Spinner() {
+  return (
+    <div className="w-full">
+      <img
+        className="mx-auto mt-4 w-20 h-20"
+        src="../../loading.gif"
+        alt="Loading..."
+      />
+    </div>
+  )
+}
+
+export default Spinner

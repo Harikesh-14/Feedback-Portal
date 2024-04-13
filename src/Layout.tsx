7578fbf9c2ff62277668bed695a2bb8fc3ddfd81
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-04-13 23:31:49 +0530

    Added Infinite scrolling

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import Header from "./container/Header"
+import Footer from "./container/Footer"
 
 function Layout() {
   return (
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   )
 }
--- src/container/CardsSection.tsx
+++ src/container/CardsSection.tsx
@@ -1,23 +1,49 @@
-import CompanyCard from "./CompanyCard"
-import { companiesData } from "../DataList/companiesDataList"
+import { useState } from "react";
+import CompanyCard from "./CompanyCard";
+import { companiesData } from "../DataList/companiesDataList";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function CardsSection() {
+  const [visibleCompanies, setVisibleCompanies] = useState(companiesData.slice(0, 5));
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchMoreData = () => {
+    setTimeout(() => {
+      const nextCompanies = companiesData.slice(
+        visibleCompanies.length,
+        visibleCompanies.length + 5 
+      );
+      if (nextCompanies.length === 0) {
+        setHasMore(false);
+      } else {
+        setVisibleCompanies([...visibleCompanies, ...nextCompanies]);
+      }
+    }, 1500);
+  };
+
   return (
-    <section className="w-11/12 mx-auto flex flex-wrap">
-      {companiesData.map((data) => (
-        data.locations.map((location, index) => (
-          <CompanyCard 
-            key={data + "-" + index}
-            companyName={data.companyName}
-            headquarter={data.headquarter}
+    <InfiniteScroll
+      dataLength={visibleCompanies.length}
+      next={fetchMoreData}
+      hasMore={hasMore}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p className="text-center text-2xl pb-4 font-semibold">No more companies to load</p>
+      }
+    >
+      {visibleCompanies.map((company) => (
+        company.locations.map((location, index) => (
+          <CompanyCard
+            key={`${company.id}-${index}`}
+            companyName={company.companyName}
+            headquarter={company.headquarter}
             location={location}
-            industry={data.industry}
+            industry={company.industry}
           />
         ))
       ))}
-      
-    </section>
-  )
+    </InfiniteScroll>
+  );
 }
 
-export default CardsSection
+export default CardsSection;

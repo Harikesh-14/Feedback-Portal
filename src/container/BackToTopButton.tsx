ff755eb0cc3366ae4604007d172e88c07538b03f
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-04-14 11:07:51 +0530

    Backt to Top button successfully working

--- src/container/BackToTopButton.tsx
+++ src/container/BackToTopButton.tsx
@@ -1,10 +1,28 @@
-import { BiUpArrowAlt } from "react-icons/bi"
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import { BiUpArrowAlt } from "react-icons/bi"
 
 function BackToTopButton() {
+  const [showButton, setShowButton] = useState<Boolean>(false)
+
+  const backToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 250) {
+      setShowButton(true)
+    } else {
+      setShowButton(false)
+    }
+  })
+
   return (
-    <div className="fixed bottom-5 right-5 flex justify-center items-center text-4xl bg-orange-500 text-white rounded-full shadow-lg opacity-80 hover:opacity-100">
-      <Link to={""} className="p-4">
+    <div className={`${showButton ? 'fixed' : 'hidden'} bottom-5 right-5 flex justify-center items-center text-4xl bg-orange-500 text-white rounded-full shadow-lg opacity-80 hover:opacity-100`}>
+      <Link to={""} className="p-4" onClick={backToTop}>
         <BiUpArrowAlt />
       </Link>
     </div>

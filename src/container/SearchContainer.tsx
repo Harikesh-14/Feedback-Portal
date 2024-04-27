f6fd9068f23b09e01c956f4e51571b2d43fbea73
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-04-27 14:09:59 +0530

    Removed the unused things from the code

--- src/container/SearchContainer.tsx
+++ src/container/SearchContainer.tsx
@@ -1,18 +1,13 @@
 import { useState } from "react"
 import { BiPaperPlane, BiSearch, BiX } from "react-icons/bi"
-import { GrLocation } from "react-icons/gr"
 import { Link } from "react-router-dom"
-import { companiesName, locationsName } from "../DataList/companiesAndLocationDataList"
+import { companiesName } from "../DataList/companiesAndLocationDataList"
 import { useSearchContext } from "../context/searchContext"
 
 function SearchContainer() {
-  // const [searchCompany, setSearchCompany] = useState<string>("")
-  // const [searchLocation, setSearchLocation] = useState<string>("")
   const { searchCriteria, setSearchCriteria } = useSearchContext();
   const [isCompanyVisible, setIsCompanyVisible] = useState<Boolean>(false)
-  const [isLocationVisible, setIsLocationVisible] = useState<Boolean>(false)
   const [suggestedCompanies, setSuggestedCompanies] = useState<string[]>([])
-  const [suggestedLocations, setSuggestedLocations] = useState<string[]>([])
 
   const handleCompaniesInputChange = (value: string) => {
     setSearchCriteria(value); // Set the search criteria in the context
@@ -23,20 +18,7 @@ function SearchContainer() {
     setSuggestedCompanies(filteredCompanies);
   };
 
-  const handleLocationInputChange = (value: string) => {
-    setSearchCriteria(value); // Set the search criteria in the context
-    const filteredLocation = locationsName
-      .filter((location) => location.locationName.toLowerCase().includes(value.toLowerCase()))
-      .map(company => company.locationName);
-
-    setSuggestedLocations(filteredLocation);
-  };
-
   const checkCompanyField = () => {
-    return searchCriteria.length === 0 ? "hidden" : "block"; // Check against searchCriteria
-  };
-
-  const checkLocationField = () => {
     return searchCriteria.length === 0 ? "hidden" : "block"; // Check against searchCriteria
   };
 
@@ -69,35 +51,6 @@ function SearchContainer() {
             <BiX
               className={`absolute top-4 right-5 text-2xl ${checkCompanyField()} hover:bg-gray-200 rounded-full cursor-pointer `}
               onClick={() => setSearchCriteria("")}
-            />
-          </span>
-        </div>
-        <div className="relative flex bg-white items-center gap-2 p-2 rounded-lg md:rounded-none md:w-[30%] shadow-md">
-          <GrLocation className="text-2xl" />
-          <input
-            type="text"
-            placeholder="Location name"
-            className="w-full outline-none h-10 px-2"
-            value={searchCriteria}
-            onChange={(e) => handleLocationInputChange(e.target.value)}
-            onFocus={() => setIsLocationVisible(true)}
-            onBlur={() => setIsLocationVisible(false)}
-          />
-          <div>
-            <ul
-              className={`absolute z-10 ${isLocationVisible ? "block" : "hidden"} ${checkLocationField()} h-auto max-h-[25rem] top-3 left-0 w-full bg-white rounded-lg shadow-md mt-12 focus:block overflow-y-auto`}
-            >
-              {suggestedLocations.map((lname, index) => (
-                <li key={index} className="p-2 hover:bg-gray-100 cursor-pointer">
-                  {lname}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <span>
-            <BiX 
-            className={`absolute top-4 right-5 text-2xl ${checkLocationField()} hover:bg-gray-200 rounded-full cursor-pointer `}
-            onClick={() => setSearchCriteria("")}
             />
           </span>
         </div>

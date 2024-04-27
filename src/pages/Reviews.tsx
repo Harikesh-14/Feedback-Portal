26f82d4bfa2c05c05404a56b7666684f6a0d819c
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-04-28 00:55:16 +0530

    Filter feature working in the review page as well

--- src/pages/Reviews.tsx
+++ src/pages/Reviews.tsx
@@ -4,9 +4,11 @@ import SearchContainer from "../container/SearchContainer";
 import ViewPartialFeedback from "../container/ViewPartialFeedback";
 import type { FeedbackT } from "../types";
 import ViewFullFeedback from "../container/ViewFullFeedback";
+import { useSearchContext } from "../context/searchContext";
 
 export default function Reviews() {
   const [feedbackData, setFeedbackData] = useState<FeedbackT>([])
+  const { searchCriteria } = useSearchContext()
 
   useEffect(() => {
     fetch("http://localhost:3000/feedback/all", {
@@ -19,11 +21,15 @@ export default function Reviews() {
     })
   }, [])
 
+  const filteredFeedbackData = feedbackData.filter((feedback) =>
+    feedback.companyName.toLowerCase().includes(searchCriteria.toLowerCase())
+  );
+
   return (
     <div>
       <SearchContainer />
       <div className="w-full flex flex-wrap justify-center items-center gap-4 p-4">
-        {feedbackData.map((feedback, index) => (
+        {filteredFeedbackData.map((feedback, index) => (
           <React.Fragment key={index}>
             <ViewPartialFeedback
             companyName={feedback.companyName}
